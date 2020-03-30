ff7c09a0b1e7e42b10e511da61d0120f70f53596
Raphael <info@therealmastermind.de>
2020-03-30 12:09:34 +0200

    Update Parse_Log_Energies.py
    Add .csv output to make life even easier

--- Parse_Log_Energies.py
+++ Parse_Log_Energies.py
@@ -21,6 +21,7 @@ subdirs = [x for x in os.listdir('.') if os.path.isdir(x)]
 subdirs.sort() # Sorting is not necessary for the program, but it simplifies readability later on.
 
 outputstring = ""
+outputcsv = ""
 if scanType == "1":
     outputstring += "Evaluating 1D Scan...\n"
     for directory in subdirs:
@@ -30,14 +31,20 @@ if scanType == "1":
         temp = open("gauss.log", "a")
         temp.close()
         with open("gauss.log", "r") as log:
-            prevtrue = False
             for line in log:
-                if searchstring in line or prevtrue:
-                    prevtrue = not prevtrue
+                if searchstring in line:
                     outputstring += line
+                    for single in line.split():
+                        try:
+                            outputcsv += str(float(single))
+                            outputcsv += "\n"
+                            break
+                        except ValueError:
+                            dummy = 0
         os.chdir('..')
 
 if scanType == "2":
+    outputcsv = "Remember that the matrix is transposed!\n"
     outputstring += "Evaluating 2D Scan...\n"
     for directory in subdirs:
         print("Directory " + directory + ":")
@@ -52,14 +59,24 @@ if scanType == "2":
             temp = open("gauss.log", "a")
             temp.close()
             with open("gauss.log", "r") as log:
-                prevtrue = False
                 for line in log:
-                    if searchstring in line or prevtrue:
-                        prevtrue = not prevtrue
+                    if searchstring in line:
                         outputstring += "\t" + line
+                        for single in line.split():
+                            try:
+                                outputcsv += str(float(single))
+                                outputcsv += ";"
+                                break
+                            except ValueError:
+                                dummy = 0
             os.chdir('..')
+        outputcsv += "\n"
         os.chdir('..')
 
 outfile = open("Energy_Summary.txt", "w")
 outfile.write(outputstring)
 outfile.close()
+
+csvfile = open("Energy_Summary.csv", "w")
+csvfile.write(outputcsv)
+csvfile.close()
